e7ddc9a4643476da1bce67c589f93f654e875c59
Camargo1958 <aldrov_camargo@hotmail.com>
2019-03-09 14:13:23 -0300

    Lesson 18 - Mores tests & Code coverage check
    Mores tests & Code coverage check added

--- server/tests/unit/validators/register.spec.js
+++ server/tests/unit/validators/register.spec.js
@@ -5,19 +5,38 @@ const { RegisterUserValidator} = validators
 describe('The RegisterUserValidator class',() => {
     describe('The validateName function ',()=>{
         test('The validateName function adds a required error to the errors array if name is not provided', ()=>{
+            
+            // ARRANGE
+
             const validator = new RegisterUserValidator({
-                email: 'bahdcoder@gma.com'
+                email: 'bahdcoder@gmail.com'
             })
 
+            //  ACTION
             validator.validateName()
 
-            const errors = validator.errors
+            // ASSERTION
+            const { errors } = validator
 
             //console.log(errors)
             expect(errors).toEqual([
                 'The name is required.'
             ])
 
+        })
+
+        test('Adds an error if name is less than 5 characters',() => {
+            // ARRANGE
+            const validator = new RegisterUserValidator({
+                name: 'haza'
+            })
+            // ACTION
+            validator.validateName()
+
+            // ASSERTION
+            expect(validator.errors).toEqual([
+                'The name must be longer than 5 characters.'
+            ])
         })
     })
 })
