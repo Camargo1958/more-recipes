e3704cdf0b14e59bee906cb96b0df0d6ae7efa96
Camargo1958 <aldrov_camargo@hotmail.com>
2019-03-26 22:05:35 -0300

    Lesson 21 - Testing is-valid async function
    Testing is-valid async function

--- server/tests/unit/validators/register.spec.js
+++ server/tests/unit/validators/register.spec.js
@@ -1,4 +1,6 @@
+import faker from 'faker'
 import validators from '../../../validators'
+import { User } from '../../../database/models'
 
 const { RegisterUserValidator} = validators
 
@@ -38,5 +40,126 @@ describe('The RegisterUserValidator class',() => {
                 'The name must be longer than 5 characters.'
             ])
         })
+
     })
+    describe('The validatePassword function ', () => {
+        test('adds a required error to the errors array if password is not provided', () => {
+            // ARRANGE
+            const validator = new RegisterUserValidator({
+              email: 'bahdcoder@gmail.com'
+            })
+      
+            // ACTION
+            validator.validatePassword()
+      
+            // ASSERTION
+            const { errors } = validator
+            expect(errors).toEqual([
+              'The password is required.'
+            ])
+        })
+
+        test('adds an error if password is less than 6 characters', () => {
+            // ARRANGE
+            const validator = new RegisterUserValidator({
+              password: 'bahd'
+            })
+            // ACTION
+      
+            validator.validatePassword()
+      
+            // ASSERTION
+            expect(validator.errors).toEqual([
+              'The password must be longer than 5 characters.'
+            ])
+        })
+    })
+    
+    describe('The validateEmail function', () => {
+        test('adds a required error if the email is not provided', async () => {
+          const validator = new RegisterUserValidator({
+            name: 'bahdcoder'
+          })
+    
+          await validator.validateEmail()
+    
+          expect(validator.errors).toEqual([
+            'The email is required.'
+          ])
+        })
+        test('adds an error if email is invalid', async () => {
+          const validator = new RegisterUserValidator({
+            email: 'bahdcoder@masd'
+          })
+    
+          await validator.validateEmail()
+    
+          expect(validator.errors).toEqual([
+            'The email must be a valid email address.'
+          ])
+        })
+    
+        test('adds an email taken error if user already exists with that email', async () => {
+            // DELETE ALL USERS FROM DB
+            await User.destroy({ where: {}})
+          
+            // CREATE NEW USER WITH THIS EMAIL
+          const user = await User.create({
+            name: 'bahdcoder',
+            //email: faker.internet.email(),
+            email: 'bahcoder@you.com',
+            password: 'password'
+          })
+    
+          // INITIALIZE OUR VALIDATOR
+          const validator = new RegisterUserValidator({
+            //email: user.email
+            email: 'bahcoder@you.com'
+          })
+    
+          await validator.validateEmail()
+    
+          expect(validator.errors).toEqual([
+            'A user with this email already exists.'
+          ])
+        })
+    })
+
+    describe('The isValid function', () => {
+        test('returns true if validation passes', async () => {
+            // DELETE ALL USERS FROM DB
+            await User.destroy({ where: {}})
+
+            // ARRANGE
+            const validator = new RegisterUserValidator({
+                name: 'bahdcoder',
+                email: 'bahdcoder@gmail.com',
+                password: 'password'
+            })
+            // ACTION
+            const result = await validator.isValid()
+
+            // ASSERTION
+            expect(result).toBe(true)
+        })
+
+        test('returns false for invalid data', async () => {
+            // DELETE ALL USERS FROM DB
+            await User.destroy({ where: {}})
+
+            // ARRANGE
+            const validator = new RegisterUserValidator({
+                name: 'bahd',
+                email: 'bahdcoder@gmail.com',
+                password: 'password'
+            })
+            // ACTION
+            const result = await validator.isValid()
+
+            // ASSERTION
+            expect(result).toBe(false)
+        })
+    })
+
+
 })
